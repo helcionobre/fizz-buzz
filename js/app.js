540dc22c8ecef789922868c05d1adda8f250f0c0
helcionobre <helcio.nobre@gmail.com>
2015-01-11 20:49:00 -0800

    Initial and final commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function() {
+
+	function fizzBuzz(par) 
+	{
+			//alert("got it");
+			//alert(par);
+			for (i = 1; i <= par; i++) 
+			{
+				if (i % 3 === 0 && i % 5 === 0) 
+				{
+					//alert("par1");
+					$(".numberlist").append("<li>FizzBuzz</li>");
+					
+				}	
+				else if (i % 3 === 0) 
+				{
+					$(".numberlist").append("<li>Fizz</li>");
+					//alert("par2");
+				}
+				else if (i % 5 === 0) 
+				{
+					$(".numberlist").append("<li>Buzz</li>");
+					//alert("par3");
+				}
+				else 
+				{
+					$(".numberlist").append("<li>" + i + "</li>");
+					//alert("par4");
+				}
+			}
+			//alert(par);
+	};
+
+	// assign number to variable after button click
+	$(".addnumber").on("click", ".addbutton", function() 
+	{
+		var value_string = document.getElementById("enternumber").value;
+		var value_number = parseInt(value_string);
+	
+	// check if enter number
+		if (isNaN(value_number)) 
+		{
+			alert ("Please enter a number");
+		}
+		else 
+		{
+			$(".numberlist").empty();
+			fizzBuzz(value_number);	
+		}
+	});
+});
